497663fb46e3bed645e8590903d7a213e1b16fc6
PlenKuing <352844752@qq.com>
2018-04-17 23:02:22 +0800

    给按钮设置了点击事件

--- app/src/main/java/com/example/plenkuing/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/plenkuing/helloandroid/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.plenkuing.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt = findViewById(R.id.bt);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello Andorid!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
